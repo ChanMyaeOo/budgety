c6f53c83ad338cb0f266e38d025e6f64b369f3c2
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-01-20 15:03:42 +0630

    get values from input fields

--- app.js
+++ app.js
@@ -6,17 +6,39 @@ let budgetController = (function(){
 
 // UI CONTROLLER
 let UIController = (function(){
-    //some code
+
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+   return{
+       getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value,   // either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+       },
+       getDOMstrings: function(){
+           return DOMstrings;
+       }
+   }
 })();
 
 
 // GLOBAL APP CONTROLLER
 let controller = (function(budgetCtrl, UICtrl){
 
+    let DOM = UICtrl.getDOMstrings();
+   
     
     let ctrlAddItem = function(){
 
         // 1. Get input field values
+        console.log(UICtrl.getInput());
 
         // 2. Add to budget controller
 
@@ -26,9 +48,9 @@ let controller = (function(budgetCtrl, UICtrl){
 
         // 5. Update UI
 
-        console.log('It works.');
+        // console.log('It works.');
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
         if(event.keyCode === 13 || event.which === 13){
